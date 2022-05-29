952e9093285c0bbbc627dd208a3a809852cd413f
salihdemir2498 <salihhdemir1727@gmail.com>
2022-05-29 21:11:36 +0300

    29 mart

--- CoreDemo/DataAccessLayer/Concrete/Context.cs
+++ CoreDemo/DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,14 @@ namespace DataAccessLayer.Concrete
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data");
+            optionsBuilder.UseSqlServer("Server=DESKTOP-4JE8ADO\\SQLEXPRESS;Database=CoreBlogDb;integrated security=true;");
         }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
     }
 }
--- CoreDemo/EntityLayer/Concrete/Blog.cs
+++ CoreDemo/EntityLayer/Concrete/Blog.cs
@@ -17,6 +17,11 @@ namespace EntityLayer.Concrete
         public string BlogImage { get; set; }
         public DateTime BlogCreateDate { get; set; }
         public bool BlogStatus { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public List<Comment> Comments { get; set; } //bir bloğa birden fazla yorum yazılabilir
+
+        //sisteme giriş yapsın öyle yorum yapılsın  istersek yazarlar ile blog tablosunu ilişkilendirmemiz gerekir
 
     }
 }
