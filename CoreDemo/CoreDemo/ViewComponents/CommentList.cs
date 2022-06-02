2f315434965c7152c536c4cef2bb47002ae6923c
salihdemir2498 <salihhdemir1727@gmail.com>
2022-06-02 13:30:19 +0300

    2 haziran

---
+++ CoreDemo/CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,35 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    Username="Salih"
+                },
+                new UserComment
+                {
+                    ID=2,
+                    Username="Döndü"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    Username="Şule"
+                },
+            };
+            return View(commentvalues);
+        }
+    }
+}
