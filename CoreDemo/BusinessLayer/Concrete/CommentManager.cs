d1ec001a4a55d1fb41ac215717db3f276a650f5b
salihdemir2498 <salihhdemir1727@gmail.com>
2022-06-05 16:04:53 +0300

    5 haziran

--- CoreDemo/BusinessLayer/Concrete/CommentManager.cs
+++ CoreDemo/BusinessLayer/Concrete/CommentManager.cs
@@ -19,7 +19,7 @@ namespace BusinessLayer.Concrete
 
         public void CommentAdd(Comment comment)
         {
-            throw new NotImplementedException();
+            _commentdal.Insert(comment);
         }
 
         public List<Comment> GetList(int id)
