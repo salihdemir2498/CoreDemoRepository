37b809dd91605c3c60dbabed81c4270f5c7a0e3a
salihdemir2498 <salihhdemir1727@gmail.com>
2022-06-10 09:01:42 +0300

    10 haziran

--- CoreDemo/CoreDemo/Startup.cs
+++ CoreDemo/CoreDemo/Startup.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +27,24 @@ namespace CoreDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddSession(); //oturum ekleme
+
+            services.AddMvc(config =>
+            {    //projeyi üye olmayanlara açýlmayacak
+                var policy = new AuthorizationPolicyBuilder()
+                              .RequireAuthenticatedUser() //kullanýcýnýn sisteme login olmasý
+                              .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services.AddMvc();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(x =>
+                {
+                    x.LoginPath = "/Login/Index";
+                }
+             );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +65,8 @@ namespace CoreDemo
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseSession();
 
             app.UseRouting();
 
