e9da43b7ee84e724abbbf7ca50365a1bbb9fe73d
salihdemir2498 <salihhdemir1727@gmail.com>
2022-06-15 14:56:35 +0300

    15 haziran

--- CoreDemo/CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/CoreDemo/Controllers/BlogController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -28,6 +31,32 @@ namespace CoreDemo.Controllers
         {
             var values=bm.GetBlogListByWriter(1);
             return View(values);
+        }
+        public IActionResult BlogAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BlogAdd(Blog p)
+        {
+            BlogValidator bv = new BlogValidator();
+            ValidationResult results = bv.Validate(p);
+            if (results.IsValid) //sonuçlar geçerliyse
+            {
+                p.BlogStatus = true;
+                p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                p.WriterId = 1;
+                bm.TAdd(p);
+                return RedirectToAction("BlogListByWriter", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
